59e9aa098ff185836a34270aadd18c8b32a0fa20
Anthony Ho <ahomnky11@gmail.com>
2016-01-28 15:28:06 +0000

    weather forecast redux

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'f039af3e4953478815429414ac92698a';
+const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+
+}
